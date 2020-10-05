14373f71fed1a88fb3c0736227c16dc6e2a52693
bskipper <baskipper67@gmail.com>
2020-10-05 14:58:47 -0500

    Added getter and setter to dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
-const Dropdown = () => {
+const Dropdown = ({options, selected, onSelectedChange}) => {
+    const renderedOptions = options.map((option) => {
+        return (
+            <div
+                key={option.value}
+                className='item'
+                onClick={() => {onSelectedChange(option)}}
+            >
+                {option.label}
+            </div>
+        )
+    })
+
     return (
-        <h1>Dropdown</h1>
+        <div className='ui form'>
+            <div className='field'>
+                <label className='label'>Select a Color</label>
+                <div className='ui selection dropdown visible active'>
+                    <i className='dropdown icon' />
+                    <div className='text'>{selected.label}</div>
+                    <div className='menu visible transition'>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
