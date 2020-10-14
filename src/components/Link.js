dca6c92f4f94f0be6097d502b52da9d1ffcd00af
bskipper <baskipper67@gmail.com>
2020-10-14 14:59:32 -0500

    nav header now updates URL on click

---
+++ src/components/Link.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Link = ({ className, href, children}) => {
+    const onClick = event => {
+        event.preventDefault()
+        window.history.pushState({}, '', href)
+    }
+    return (
+        <a
+            className={className}
+            href={href}
+            onClick={onClick}
+        >
+            {children}
+        </a>
+    )
+}
+
+export default Link
