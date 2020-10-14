746b863e31b50a7f4edbe581cf509404e7196250
bskipper <baskipper67@gmail.com>
2020-10-14 14:02:27 -0500

    Created basic navigation component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const items = [
     {
@@ -35,17 +36,26 @@ const options = [
 ]
 
 export default () => {
-    // const [selected, setSelected] = useState(options[0])
+    const [selected, setSelected] = useState(options[0])
   return (
       <div>
-        {/*<Accordion items={items}/>*/}
-        {/*<Search />*/}
-        <Translate />
-        {/*<Dropdown*/}
-        {/*    selected={selected}*/}
-        {/*    onSelectedChange={setSelected}*/}
-        {/*    options={options}*/}
-        {/*/>*/}
+        <Route path='/'>
+            <Accordion items={items} />
+        </Route>
+        <Route path='/list'>
+            <Search />
+        </Route>
+        <Route path='/dropdown'>
+            <Dropdown
+                label='Select a colour'
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected()}
+            />
+        </Route>
+        <Route path='/translate'>
+            <Translate items={items} />
+        </Route>
       </div>
   )
 }
