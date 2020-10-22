9d225cbd97b585353dabd11fbd4b2e69d20f49fa
bskipper <baskipper67@gmail.com>
2020-10-22 14:41:56 -0500

    attached thunk to api call

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import {createStore, applyMiddleware } from "redux";
+
 import App from './components/App';
-import {createStore} from "redux";
 import reducers from './store/reducers'
 
+const store = createStore(reducers, applyMiddleware(thunk))
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
-    <App />
+    <Provider store={store}>
+        <App />
     </Provider>,
 document.getElementById('root')
 );
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
+import jsonPlaceholder from "../../APIs/jsonPlaceholder";
+
+export const fetchPosts = () =>
+    async (dispatch, getState) => {
+        const response = await jsonPlaceholder.get('/posts')
+        dispatch({
+            type: 'FETCH_POSTS',
+            payload: response
+        })
     }
-}
+
