db0d53013ea973879e711d6a101f51470cbc482e
bskipper <baskipper67@gmail.com>
2020-10-29 14:56:19 -0500

    Rendering posts now

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,16 +2,33 @@ import React, {useEffect} from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts} from "../store/actions";
 
-const PostList = ({fetchPosts}) => {
+const PostList = ({fetchPosts, posts}) => {
     useEffect(() => {
         fetchPosts()
     }, [])
 
+    const renderList = () =>
+        posts.map(post =>
+        <div className='item' key={post}>
+            <i className="large middle aligned icon user" />
+            <div className="content">
+                <div className="description">
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+            </div>
+        </div>
+        )
+
     return (
         <div>
-            PostList
+            {renderList()}
         </div>
     )
 }
 
-export default connect(null, {fetchPosts})(PostList)
+const mapStateToProps = ({posts}) => {
+    return {posts}
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostList)
